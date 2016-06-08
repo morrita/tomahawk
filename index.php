955af546946895ddc842c17fe6141d26525971d1
Tom Morris <tom_morris@btinternet.com>
2016-06-08 18:29:03 +0100

    update made

--- index.php
+++ index.php
@@ -2,20 +2,28 @@
 
 get_header();
 
-
 if ( have_posts() ): 
-  while ( have_posts() ): 
-    the_post();
+  while (have_posts()):
  
-    echo "<h2>";
+    the_post();
+
+    echo '<article class="post">';
+ 
+    echo '<h2><a href="';
+    the_permalink();
+    echo '">';
     the_title();
-    echo "</h2>";
-   
+    echo '</a></h2>';
+
     the_content();
+    echo '</article>';
 
   endwhile; 
+
 else:
+
   echo '<p>Sorry, no posts matched your criteria</p>';
+
 endif; 
 
 get_footer(); 

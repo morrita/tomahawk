08b257e7bd357ef5f92d5ad3278db592905ece26
Tom Morris <tom_morris@btinternet.com>
2016-06-12 11:37:01 +0100

    update made

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 	<footer class="site-footer">
 	
 		<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y');?></p>
+
+           <nav class="site-nav">
+                           <?php wp_nav_menu(); ?>
+                        </nav>
 	
 	</footer>
 
